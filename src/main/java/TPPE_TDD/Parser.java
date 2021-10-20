235907b7633d4b93edf12b3b8b9b02f990f57b07
Marcos <marcosnery.comp@gmail.com>
2021-10-20 02:23:27 -0300

    leitura: teste falhando por unidade nao implementada

---
+++ src/main/java/TPPE_TDD/Parser.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TPPE_TDD;
+
+/**
+ *
+ * @author marcos
+ */
+public class Parser {
+
+   private String arquivoEntrada;
+   private String arquivoSaida;
+
+    public Parser(String arquivoEntrada, String arquivoSaida) {
+        this.arquivoEntrada = arquivoEntrada;
+        this.arquivoSaida = arquivoSaida;
+    }
+
+   
+    
+    
+}
---
+++ src/test/java/TPPE_TDD/ParserTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TPPE_TDD;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author marcos
+ */
+public class ParserTest {
+    
+
+    @Test
+    public void testLeArquivo() {
+        Parser parser = new Parser("testFixture.txt","results.txt");
+        String fileContents = parser.readInput();
+    }
+    
+}
